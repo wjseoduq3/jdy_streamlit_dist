5b96ac502213a3702fcbe1d745ddd4240b1e08e2
wjseoduq3 <166124628+wjseoduq3@users.noreply.github.com>
2024-06-13 15:32:26 +0900

    Add files via upload
    img_folder modify

--- sidebar07.py
+++ sidebar07.py
@@ -15,12 +15,13 @@ st.sidebar.write('선택한 작품은:', user_opt)
 
 # 메인화면
 st.title('스트림릿의 사이드바')
-folder =r'D:/JDY_folder/vs_ws/data/'
+# folder =r'D:/JDY_folder/vs_ws/data/'
 image_files=['Vermeer.png','Gogh.png','Munch.png','ShinYoonbok.png']
 
 sel_img_index = sel_opt.index(user_opt)
 
 # 선택한 항목에 맞는 이미지 파일 지정
 img_file = image_files[sel_img_index]
+# img_local = Image.open(folder+img_file)
 img_local = Image.open(folder+img_file)
 st.image(img_local,caption=user_opt)
